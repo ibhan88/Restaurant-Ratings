7972bbbea38b9b0e6f2e8c9990b7645fef39c788
Hackbright Student <info@hackbrightacademy.com>
2016-07-11 17:07:22 -0700

    Organized into Functions.

--- restaurant-ratings.py
+++ restaurant-ratings.py
@@ -1,31 +1,38 @@
 # your code goes here
 import sys 
 
-#Open the file containing restaurant names and ratings.
-ratings_file = open(sys.argv[1])
+def sort_restaurants(filename):
+    #Open the file containing restaurant names and ratings.
+    ratings_file = open(filename)
 
-#Initialize dictionary as empty.
-restaurants = {}
+    #Initialize dictionary as empty.
+    restaurants = {}
 
-#Loop over the restaurant ratings file
-for line in ratings_file:
+    #Loop over the restaurant ratings file
+    for line in ratings_file:
 
-    #Create list containing one restaurant name and rating
-    line = line.rstrip()
-    line = line.split(":")
+        #Create list containing one restaurant name and rating
+        line = line.rstrip()
+        line = line.split(":")
 
-    #line[0] is the restaurant name; line[1] is the rating. 
-    #Use the name as the key and the rating as the value.
-    restaurants[line[0]] = line[1]
+        #line[0] is the restaurant name; line[1] is the rating. 
+        #Use the name as the key and the rating as the value.
+        restaurants[line[0]] = line[1]
 
-#Convert dictionary into list of tuples.
-#Sort restaurants alphabetically.
-sorted_restaurants = sorted(restaurants.items())
+    #Convert dictionary into list of tuples.
+    #Sort restaurants alphabetically.
+    return sorted(restaurants.items())
 
-#Loop over sorted list.
-for restaurant in sorted_restaurants:
-    restaurant_name = restaurant[0]
-    restaurant_rating = restaurant[1]
+def print_restaurant_ratings():
 
-    print "{} is rated at {}.".format(restaurant_name, restaurant_rating)
+    sorted_restaurants = sort_restaurants(sys.argv[1])
+
+    #Loop over sorted restaurants.
+    for restaurant in sorted_restaurants:
+        restaurant_name = restaurant[0]
+        restaurant_rating = restaurant[1]
+
+        print "{} is rated at {}.".format(restaurant_name, restaurant_rating)
+
+print_restaurant_ratings()
 
